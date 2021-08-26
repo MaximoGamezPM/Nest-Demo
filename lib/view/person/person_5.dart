9ea3a2c8d5ffe6b20b289f958b39013b0a477bdd
Parabeac-Nest <admin@parabeac.com>
2021-08-26 05:20:44 +0000

    Auto-generated commit by Parabeac Nest

---
+++ lib/view/person/person_5.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import '../person/person_5.dart';
+import '../person/person_frame.dart';
+
+class Person5 extends StatelessWidget {
+  final constraints;
+
+  Person5(
+    this.constraints, {
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      'assets/images/0_38.png',
+      width: constraints.maxWidth * 1.000,
+      height: constraints.maxHeight * 1.000,
+    );
+  }
+}
